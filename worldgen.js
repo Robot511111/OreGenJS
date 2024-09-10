c26a84dcee10608475b0f593306a6ea7d107e679
Robot511111 <139191562+Robot511111@users.noreply.github.com>
2024-09-10 17:29:00 -0400

    Add files via upload

---
+++ worldgen.js
@@ -0,0 +1,36 @@
+//made by InspectorBlue https://legacy.curseforge.com/minecraft/data-packs/oregenjs/files
+
+WorldgenEvents.remove(event => {
+    event.removeOres(props => {
+        props.worldgenLayer = 'underground_ores' ;
+        props.blocks = [
+        'railcraft:lead_ore',
+        'railcraft:deepslate_lead_ore',
+        'railcraft:nickel_ore',
+        'railcraft:deepslate_nickel_ore',
+        'railcraft:tin_ore',
+        'railcraft:deepslate_tin_ore',
+        'railcraft:silver_ore',
+        'railcraft:deepslate_silver_ore',
+        'railcraft:zinc_ore',
+        'railcraft:deepslate_zinc_ore',
+        'create:zinc_ore',
+        'create:deepslate_zinc_ore',
+        'occultism:silver_ore',
+        'occultism:silver_ore_deepslate',
+        'eidolon:lead_ore',
+        'eidolon:deep_lead_ore',
+        'eidolon:silver_ore',
+        'eidolon:deep_silver_ore',
+        'immersiveengineering:ore_aluminum',
+        'immersiveengineering:deepslate_ore_aluminum',
+        'iceandfire:silver_ore',
+        'iceandfire:deepslate_silver_ore',
+        'treasure2:ruby_ore',
+        'treasure2:deepslate_ruby_ore',
+        'treasure2:sapphire_ore',
+        'treasure2:deepslate_sapphire_ore',
+        'voluminousenergy:galena_ore',
+        'voluminousenergy:deepslate_galena_ore'
+    ]})
+})
